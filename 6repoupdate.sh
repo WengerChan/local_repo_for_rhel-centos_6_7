137709faa6a6c6e696e9e7bd5bbac9783bdbfd6f
WengerChan <37797982+WengerChan@users.noreply.github.com>
2020-12-18 13:32:46 +0800

    Revert "init commit"
    This reverts commit ffff2342b2ac1f8222083047234e8892e4af060c.

--- 6repoupdate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-download_path="/repos"
-
-reposync --plugins --newest-only --delete --download_path=$download_path --repoid=rhel-6-server-rpms
-reposync --plugins --newest-only --delete --download_path=$download_path --repoid=rhel-6-server-extras-rpms
-reposync --plugins --newest-only --delete --download_path=$download_path --repoid=rhel-ha-for-rhel-6-server-rpms
-reposync --plugins --newest-only --delete --download_path=$download_path --repoid=rhel-rs-for-rhel-6-server-rpms
