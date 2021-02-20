b871d8a8517d881b5806438430cc68c3ffaf121c
WengerChan <37797982+WengerChan@users.noreply.github.com>
2021-02-20 09:09:37 +0800

    Update 8_repo_createrepo.sh

--- 8_repo_createrepo.sh
+++ 8_repo_createrepo.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-rm -rf /repos/rhel-8-for-x86_64-appstream-rpms/repodata
-rm -rf /repos/rhel-8-for-x86_64-baseos-rpms/repodata
-rm -rf /repos/rhel-8-for-x86_64-highavailability-rpms/repodata
-rm -rf /repos/rhel-8-for-x86_64-resilientstorage-rpms/repodata
-rm -rf /repos/rhel-8-for-x86_64-appstream-debug-rpms/repodata
-rm -rf /repos/rhel-8-for-x86_64-baseos-debug-rpms/repodata
-rm -rf /repos/epel/repodata
-rm -rf /repos/epel-modular/repodata
-rm -rf /repos/codeready-builder-for-rhel-8-x86_64-rpms/repodata
-rm -rf /repos/ansible-2-for-rhel-8-x86_64-rpms/repodata
-rm -rf /repos/ansible-2-for-rhel-8-x86_64-debug-rpms/repodata
+rm -rf /repos/rhel-8-for-x86_64-appstream-rpms/{.,}repodata
+rm -rf /repos/rhel-8-for-x86_64-baseos-rpms/{.,}repodata
+rm -rf /repos/rhel-8-for-x86_64-highavailability-rpms/{.,}repodata
+rm -rf /repos/rhel-8-for-x86_64-resilientstorage-rpms/{.,}repodata
+rm -rf /repos/rhel-8-for-x86_64-appstream-debug-rpms/{.,}repodata
+rm -rf /repos/rhel-8-for-x86_64-baseos-debug-rpms/{.,}repodata
+rm -rf /repos/epel/{.,}repodata
+rm -rf /repos/epel-modular/{.,}repodata
+rm -rf /repos/codeready-builder-for-rhel-8-x86_64-rpms/{.,}repodata
+rm -rf /repos/ansible-2-for-rhel-8-x86_64-rpms/{.,}repodata
+rm -rf /repos/ansible-2-for-rhel-8-x86_64-debug-rpms/{.,}repodata
 
 rm -f /repos/rhel-8-for-x86_64-appstream-rpms/modules.yaml
 rm -f /repos/rhel-8-for-x86_64-baseos-rpms/modules.yaml
